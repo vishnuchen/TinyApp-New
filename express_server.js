39c5fc4164ef3b188760dea6a9b0bf0d1f97c0e8
Vishnu Chennuru <vishnuchen@gmail.com>
2019-06-05 00:03:17 +0000

    added random string

--- express_server.js
+++ express_server.js
@@ -4,10 +4,18 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+    let shortURL = (math.random().toString(36).replace(/[^a-z]+/g, '').substr(0,5));
+    return shortURL
+}
 
 app.get("/", (req, res) => {
     res.send("Hello!");
@@ -38,6 +46,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
